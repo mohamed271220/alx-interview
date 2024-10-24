488673f7c34018848bd413c2e508c64fe9c6a8f8
mohamed271220 <mohamedaprahym@gmail.com>
2024-10-24 06:54:38 +0300

    s

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -23,16 +23,14 @@ def print_stats(dict_sc, total_file_size):
 total_file_size = 0
 code = 0
 counter = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0,}
 
 try:
     for line in sys.stdin:
