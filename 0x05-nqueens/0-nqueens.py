852bf260acf8dcc1192d816352b896fc21cb96fe
mohamed271220 <mohamedaprahym@gmail.com>
2024-11-14 08:12:24 +0200

    done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -44,3 +44,35 @@ def solve(n):
 
 
 solve(n)
+
+# def solveQueens(n:int) -> List[List[str]]:
+#     col = set()
+#     posDiag = set() # row + col
+#     negDiag = set() # row - col
+#     res = []
+#     board = [['.' for _ in range(n)] for _ in range(n)]
+#     def backtrack(r):
+#         if r == n:
+#             res.append([''.join(row) for row in board])
+#             return
+#         for c in range(n):
+#             if c in col or (r+c) in posDiag or (r-c) in negDiag:
+#                 continue
+#             col.add(c)
+#             posDiag.add(r+c)
+#             negDiag.add(r-c)
+#             board[r][c] = 'Q'
+#             backtrack(r+1)
+#             col.remove(c)
+#             posDiag.remove(r+c)
+#             negDiag.remove(r-c)
+#             board[r][c] = '.'
+#     backtrack(0)
+#     return res
+
+# # to print every possible solution
+# def printSolution(board):
+#     for i in range(len(board)):
+#         for j in range(len(board)):
+#             print(board[i][j], end = " ")
+#         print()
