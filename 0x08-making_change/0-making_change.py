93c1106328a46c511f752a9c1a780a53d61f0d8d
mohamed271220 <mohamedaprahym@gmail.com>
2024-12-05 13:24:39 +0200

    s

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -24,3 +24,33 @@ def makeChange(coins, total):
         if total == 0:
             return change
     return -1
+
+# /**
+#  * A function that returns the fewest number of coins needed to meet a total
+#  */
+
+# function makeChange(coins: number[], total: number): number {
+#     /**
+#      * Returns: fewest number of coins needed to meet total
+#      * If total is 0 or less, return 0
+#      * If total cannot be met by any number of coins you have, return -1
+#      */
+#     if (!coins || coins.length === 0) {
+#         return -1;
+#     }
+#     if (total <= 0) {
+#         return 0;
+#     }
+#     let change = 0;
+#     coins = coins.sort((a, b) => b - a);
+#     for (let coin of coins) {
+#         while (coin <= total) {
+#             total -= coin;
+#             change += 1;
+#         }
+#         if (total === 0) {
+#             return change;
+#         }
+#     }
+#     return -1;
+# }
